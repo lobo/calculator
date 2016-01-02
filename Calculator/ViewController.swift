ece95c4c521f2eb6e6f510096e97687a9fcbe395
Daniel Lobo <dlobo@itba.edu.ar>
2016-01-02 02:22:03 -0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Daniel Lobo on 1/2/16.
+//  Copyright © 2016 Daniel Lobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
